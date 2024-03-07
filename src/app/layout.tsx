d4b9186f84f14b73bfd86c15c6fab80895190bce
ridwanmubarok <ridwanmubarok5@gmail.com>
2024-03-07 19:25:46 +0700

    remove providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
-import Providers from "@/lib/provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
